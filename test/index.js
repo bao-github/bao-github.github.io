8f109747c29c154d2f8484bb3457fcc97bec9e8c
nieshubao <nieshubao@cn.panasonic.com>
2025-04-17 10:47:14 +0800

    1

--- test/index.js
+++ test/index.js
@@ -1,10 +1,19 @@
-async function print() {
+async function conn1() {
 
   var myDisplayStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
 
-  document.getElementById("local_video").srcObject = myDisplayStream;
+  document.getElementById("local_video1").srcObject = myDisplayStream;
 }
 
+async function conn2() {
+
+  var myDisplayStream = await navigator.mediaDevices.getDisplayMedia({
+    audio: true,
+    video: true,
+  });
+
+  document.getElementById("local_video2").srcObject = myDisplayStream;
+}
