fd7300ab86ee10e07f7371941d474a03b7f37229
nieshubao <nieshubao@cn.panasonic.com>
2025-04-21 16:06:20 +0800

    1

--- common.js
+++ common.js
@@ -1,6 +1,4 @@
 var comm_pages=`
     <a href="./index.html">index</a>
-    <a href="./pwd.html">pwd</a>
-    <a href="./code.html">code</a>
-    <a href="./log.html">log</a>
+    <a href="./password.html">password</a>
 `;
