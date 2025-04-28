95da6b4a2411f64a848b55a6b207957d9e3108cd
nieshubao <nieshubao@cn.panasonic.com>
2025-04-28 08:33:19 +0800

    1

--- common.js
+++ common.js
@@ -1,8 +1,9 @@
 var comm_pages = `
-    <a href="./index.html">index</a>
-    <a href="./password.html">password</a>
-    <a href="./text.html">text</a>
-    |
     <a href="#page-pos-0">&lt;top&gt;</a>
+    |
+    <a href="./index.html">index</a>
+    <a href="./index-2.html">index-2</a>
+    <a href="./password.html">password</a>
+    |
     <a href="#page-pos-1">&lt;bottom&gt;</a>
 `;
