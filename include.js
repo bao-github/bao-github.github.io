58173126e33497958ad3846d0c4684237a62171c
nieshubao <nieshubao@cn.panasonic.com>
2025-04-28 18:26:18 +0800

    1

--- include.js
+++ include.js
@@ -57,7 +57,8 @@ var comm_pages = `
     <a href="#page-pos-0">&lt;top&gt;</a>
     |
     <a href="./index.html">index</a>
-    <a href="./index-2.html">index-2</a>
+    <a href="./text-1.html">text-1</a>
+    <a href="./text-2.html">text-2</a>
     <a href="./password.html">password</a>
     <a href="./bookmarks.html">bookmarks</a>
     |
