145b53ab23ef3fad2503a657f60a01d60f6adae7
nieshubao <nieshubao@cn.panasonic.com>
2025-07-02 17:45:14 +0800

    1

--- include.js
+++ include.js
@@ -226,6 +226,11 @@ $(function () {
         color: #000000;
         border-radius: 4px;
         border: 1px solid #999999;
+        appearance: none;
+        background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23999'%3e%3cpath d='M7 10l5 5 5-5z'/%3e%3c/svg%3e");
+        background-repeat: no-repeat;
+        background-position: right 5px center;
+        padding-right: 30px;
       }
 
       select:focus-visible {
