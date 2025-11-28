b183e1a9df4bad67bd857e2d292041be3fd36dfc
nieshubao <nieshubao@cn.panasonic.com>
2025-11-28 13:58:43 +0800

    1

--- cgh/cgh-1.user.js
+++ cgh/cgh-1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         s-1
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  s-1
 // @match        https://www.baidu.com/*
 // @grant        none
@@ -11,6 +11,6 @@
   "use strict";
 
   setTimeout(() => {
-    alert("123456");
-  }, 3000);
+    alert("0.2");
+  }, 5000);
 })();
